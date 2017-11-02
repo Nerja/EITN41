0d778913ba7e56bec6ad45655e3b83a064f75aee
lthd92 <dat13njo@student.lu.se>
2017-11-03 00:09:14 +0100

    Assignment 2 Skeleton

---
+++ Assignments/Assignment2/assignment2.py
@@ -0,0 +1,9 @@
+__author__ = "Marcus Rodan & Niklas Jönsson"
+
+import simulation as s
+
+def main():
+    s.start_simulation(1, 1, 1)
+
+if __name__ == "__main__":
+    main()
---
+++ Assignments/Assignment2/simulation.py
@@ -0,0 +1,4 @@
+__author__ = "Marcus Rodan & Niklas Jönsson"
+
+def start_simulation(u, k, c):
+    print("Starting simulation with u={}, k={} and c={}".format(u, k, c))
