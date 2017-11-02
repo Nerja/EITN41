85ce11150f914e746588132fb6f2a5166a50ec37
Marcus Rodan <marcusrodan@gmail.com>
2017-11-02 23:41:59 +0100

    luhn

--- Assignments/Assignment1_niklas/luhn.py
+++ Assignments/Assignment1_niklas/luhn.py
@@ -1,19 +1,22 @@
-__author__ = "Niklas Jönsson"
+__author__ = "Marcus Rodan"
 
-def luhn_test(card_number):
-    return True
+def luhn(nbrs_string):
+    nbrs        = list(map(int, nbrs_string))
+    sum         = 0
+    for i in range(len(nbrs)):
+        nbr = 2*nbrs[i] if i%2 == 0 else nbrs[i]
+        sum += (nbr - 9) if nbr > 9 else nbr
+    return sum % 10
 
-#function checkLuhn(string purportedCC) {
-#     int sum := 0
-#     int nDigits := length(purportedCC)
-#     int parity := nDigits modulus 2
-#     for i from 0 to nDigits - 1 {
-#         int digit := integer(purportedCC[i])
-#         if i modulus 2 = parity
-#             digit := digit × 2
-#             if digit > 9
-#                 digit := digit - 9
-#         sum := sum + digit
-#     }
-#     return (sum modulus 10) = 0
- #}
+def luhn_test(nbrs_string):
+    return luhn(nbrs_string) == 0
+
+if __name__ == "__main__":
+    print(luhn("1234567890") == 3)
+    print(luhn("5234567890") == 2)
+    print(luhn("5234567893") == 5)
+    print(luhn("1277421285754109") == 0)
+    print(luhn("5866044108627571") == 0)
+    print(luhn("7473986953606632") == 0)
+    print(luhn("4026467645830632") == 0)
+    print(luhn("2033092648604969") == 0)
