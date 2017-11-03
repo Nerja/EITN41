8e8b3cb942e9c73d91b3deae84a1e59be8925195
Marcus Rodan <marcusrodan@gmail.com>
2017-11-03 11:48:16 +0100

    paper, refac

--- Assignments/Assignment1/MicroMint/main.py
+++ Assignments/Assignment1/MicroMint/main.py
@@ -3,13 +3,23 @@ __author__ = "Marcus Rodan & Niklas Jönsson"
 import simulation as s
 import numpy as np
 import sys
+
+def run_case(u, k, c):
+    num_runs = 100
+    lam = 3.77
+    results = []
+    print("Starting simulation with u={}, k={} and c={}".format(u, k, c))
+    for i in range(0, num_runs):
+        results.append(s.start_simulation(u, k, c))
+        sys.stdout.write('.')
+        sys.stdout.flush()
+    print()
+    print("Mean Coin Throws: {}".format(np.mean(results)))
+
 def main():
 
     num_coins_to_test = [1, 100, 10000]
     u_k = [[16, 20], [2, 7]]
-
-    num_runs = 100
-    lam = 3.77
 
     for i in range(0, len(u_k)):
         u = u_k[0][i]
@@ -17,14 +27,7 @@ def main():
         for j in range(0, len(num_coins_to_test)):
             results = []
             c = num_coins_to_test[j]
-            print("Starting simulation with u={}, k={} and c={}".format(u, k, c))
-            for i in range(0, num_runs):
-                results.append(s.start_simulation(u, k, c))
-                sys.stdout.write('.')
-                sys.stdout.flush()
-            print()
-            print("Mean Coin Throws: {}".format(np.mean(results)))
-
+            run_case(u, k, c)
 
 if __name__ == "__main__":
     main()
--- Assignments/Assignment1/MicroMint/simulation.py
+++ Assignments/Assignment1/MicroMint/simulation.py
@@ -9,13 +9,13 @@ def start_simulation(u, k, c):
     coin_threshold = k
     num_coins_to_generate = c
     coins_generated = 0
-    coins_thrown = 0
+    balls_thrown = 0
 
     while coins_generated < num_coins_to_generate:
-        coins_thrown += 1
+        balls_thrown += 1
         bin_nbr = random.randint(0, len(bins) - 1)
         bins[bin_nbr] += 1
         if bins[bin_nbr] >= coin_threshold:
             coins_generated += 1
 
-    return coins_thrown
+    return balls_thrown
