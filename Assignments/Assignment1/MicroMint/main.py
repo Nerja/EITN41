541b772c8e162ff5e23c45868461fa74d9a7e54d
Marcus Rodan <marcusrodan@gmail.com>
2017-11-03 12:41:28 +0100

    fix

--- Assignments/Assignment1/MicroMint/main.py
+++ Assignments/Assignment1/MicroMint/main.py
@@ -5,7 +5,7 @@ import numpy as np
 import sys
 
 def compute_conf_int(observations):
-    lam = 3.77
+    lam = 3.66
     n   = len(observations)
     return 2 * lam * np.std(np.array(observations))/(n ** 0.5)
     
@@ -20,7 +20,7 @@ def run_case(u, k, c, des_width):
         i = i + 1
         if conf_width < des_width and i > 2:
             run = False
-    print("Mean Coin Throws: {}".format(np.mean(results)))
+    print("Mean Coin Throws: {} used {} iterations".format(np.mean(results), i))
 
 def main():
     params_list = [(16, 2, 1, 22), (16, 2, 100, 24), (16, 2, 10000, 22), (20, 7, 1, 79671), (20, 7, 100, 15616), (20, 7, 10000, 4783)]
--- Assignments/Assignment1/MicroMint/simulation.py
+++ Assignments/Assignment1/MicroMint/simulation.py
@@ -10,12 +10,14 @@ def start_simulation(u, k, c):
     num_coins_to_generate = c
     coins_generated = 0
     balls_thrown = 0
+    bin_generated = set()
 
     while coins_generated < num_coins_to_generate:
         balls_thrown += 1
         bin_nbr = random.randint(0, len(bins) - 1)
         bins[bin_nbr] += 1
-        if bins[bin_nbr] >= coin_threshold:
+        if bins[bin_nbr] >= coin_threshold and not bin_nbr in bin_generated:
             coins_generated += 1
+            bin_generated.add(bin_nbr)
 
     return balls_thrown
