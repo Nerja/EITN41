d1f4c5a381a5cd27562c3f2373c0c78c0983cea0
lthd92 <dat13njo@student.lu.se>
2017-11-03 01:08:52 +0100

    Assignment 2 klar, förutom COnfidence Intervall

--- Assignments/Assignment1/MicroMint/main.py
+++ Assignments/Assignment1/MicroMint/main.py
@@ -1,9 +1,30 @@
 __author__ = "Marcus Rodan & Niklas Jönsson"
 
 import simulation as s
-
+import numpy as np
+import sys
 def main():
-    s.start_simulation(1, 1, 1)
+
+    num_coins_to_test = [1, 100, 10000]
+    u_k = [[16, 20], [2, 7]]
+
+    num_runs = 100
+    lam = 3.77
+
+    for i in range(0, len(u_k)):
+        u = u_k[0][i]
+        k = u_k[1][i]
+        for j in range(0, len(num_coins_to_test)):
+            results = []
+            c = num_coins_to_test[j]
+            print("Starting simulation with u={}, k={} and c={}".format(u, k, c))
+            for i in range(0, num_runs):
+                results.append(s.start_simulation(u, k, c))
+                sys.stdout.write('.')
+                sys.stdout.flush()
+            print()
+            print("Mean Coin Throws: {}".format(np.mean(results)))
+
 
 if __name__ == "__main__":
     main()
--- Assignments/Assignment1/MicroMint/simulation.py
+++ Assignments/Assignment1/MicroMint/simulation.py
@@ -1,4 +1,21 @@
 __author__ = "Marcus Rodan & Niklas Jönsson"
 
+import random
+
 def start_simulation(u, k, c):
-    print("Starting simulation with u={}, k={} and c={}".format(u, k, c))
+
+    #Skapa en array med 2^u platser(bins)
+    bins = [0] * (2 ** u)
+    coin_threshold = k
+    num_coins_to_generate = c
+    coins_generated = 0
+    coins_thrown = 0
+
+    while coins_generated < num_coins_to_generate:
+        coins_thrown += 1
+        bin_nbr = random.randint(0, len(bins) - 1)
+        bins[bin_nbr] += 1
+        if bins[bin_nbr] >= coin_threshold:
+            coins_generated += 1
+
+    return coins_thrown
