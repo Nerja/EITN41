be25a6a51cc6f900786c9889e5639cc84215ab0e
Marcus Rodan <marcusrodan@gmail.com>
2017-11-03 13:00:33 +0100

    quiz test

---
+++ Assignments/Assignment1/MicroMint/quiz.py
@@ -0,0 +1,7 @@
+import main
+
+__author__ = "Marcus Rodan & Niklas Jönsson"
+
+if __name__ == "__main__":
+    print("c = 10000, u = 20, k = 7, width=4783 = {}".format(main.run_case(20, 7, 10000, 4783)))
+    print("c = 1, u = 16, k = 2, width=22 = {}".format(main.run_case(16, 2, 1, 22)))
