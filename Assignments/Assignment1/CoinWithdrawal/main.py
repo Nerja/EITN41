496770fe920fe0b565c6ba94e1f32b28d42c9449
lthd92 <dat13njo@student.lu.se>
2017-11-07 21:48:56 +0100

    gcd metod

--- Assignments/Assignment1/CoinWithdrawal/main.py
+++ Assignments/Assignment1/CoinWithdrawal/main.py
@@ -1,14 +1,16 @@
 import random as r
-
+import math as m
 __author__ = "Marcus Rodan & Niklas Jönsson"
 
 def generate_2k(k, n):
     return [(r.randint(1, n), r.randint(1, n), r.randint(1, n), r.randint(1, n)) for i in range(2*k)]
 
 def compute_xi(quad):
+    print(quad)
     return 1
 
 def compute_yi(quad, id):
+    #print(quad + id)
     return 2
 
 def compute_xi_yi(quads, id):
@@ -16,17 +18,21 @@ def compute_xi_yi(quads, id):
     y = list(map(lambda quad: compute_yi(quad, id), quads))
     return x, y
 
+def gcd(a, b):
+    return m.gcd(a, b)
+
 def main():
     k   = 2
     n   = 1000000000
     id  = 1337
-    
+
     # Client generates 2k (ai, ci, di, ri)
     quads = generate_2k(k, n)
     #print(quads)
 
     # Generate xi, yi for quads
     x, y = compute_xi_yi(quads, id)
+
     #print(x, y)
 
 if __name__ == "__main__":
