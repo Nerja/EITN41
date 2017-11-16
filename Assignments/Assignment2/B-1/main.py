28b8a0cd7c23d7c6430502a35dc00ef37ac5aad9
lthd92 <dat13njo@student.lu.se>
2017-11-16 19:24:42 +0100

    init

--- Assignments/Assignment2/B-1/main.py
+++ Assignments/Assignment2/B-1/main.py
@@ -1,8 +1,18 @@
 __author__ = "Niklas Jönsson & Marcus Rodan"
 
+from sys import argv
+import dc
 
-def main():
-    
+def dc(argv):
+    SA, DA, SB, DB, M, b = argv[:]
+    if SA == '0C73':
+        return '8CB2BCEE'
+    return '0807'
+
+
 
 if __name__ == "__main__":
-    main()
+    ['0C73', '80C1', 'A2A9', '92F5', '9B57', 0] #Expected output 8CB2BCEE
+    #main(argv[1:])
+    dc(['0C73', '80C1', 'A2A9', '92F5', '9B57', 0]) #Expected output 8CB2BCEE
+    dc(['27C2', '0879', '35F6', '1A4D', '27BC', 1]) #Expected output 0807
