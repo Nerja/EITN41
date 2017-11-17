40c6874df48d9174a1e8665638fdd45cbcffb5be
lthd92 <dat13njo@student.lu.se>
2017-11-17 17:55:24 +0100

    b-2 init

--- Assignments/Assignment2/B-2/main.py
+++ Assignments/Assignment2/B-2/main.py
@@ -7,8 +7,14 @@ def main(nazir_ip, mix_ip, nbr_partners, data):
     capfile = savefile.load_savefile(testcap, layers=2, verbose=True)
 
     # print the packets
-    print ('timestamp\teth src\t\t\teth dst\t\t\tIP src\t\tIP dst')
-    for pkt in capfile.packets:
+    #print ('timestamp\teth src\t\t\teth dst\t\t\tIP src\t\tIP dst')
+    learn(nazir_ip, capfile, nbr_partners)
+
+    #print ('{}\t\t{}\t{}\t{}\t{}'.format(timestamp, eth_src, eth_dst, ip_src, ip_dst))
+
+
+def learn(nazir_ip, data, m):
+    for pkt in data.packets:
         timestamp = pkt.timestamp
         # all data is ASCII encoded (byte arrays). If we want to compare with strings
         # we need to decode the byte arrays into UTF8 coded strings
@@ -16,8 +22,10 @@ def main(nazir_ip, mix_ip, nbr_partners, data):
         eth_dst = pkt.packet.dst.decode('UTF8')
         ip_src = pkt.packet.payload.src.decode('UTF8')
         ip_dst = pkt.packet.payload.dst.decode('UTF8')
-        print ('{}\t\t{}\t{}\t{}\t{}'.format(timestamp, eth_src, eth_dst, ip_src, ip_dst))
 
+        if ip_src == nazir_ip:
+            #look at all ip_dst at a current timestamp and save this to a set
+            print ('{}\t\t{}\t{}\t{}\t{}'.format(timestamp, eth_src, eth_dst, ip_src, ip_dst))
 
 if __name__ == "__main__":
-    main(0,0,0, 'test.pcap')
+    main('159.237.13.37', '94.147.150.188', 2, 'cia.log.1337.pcap')
