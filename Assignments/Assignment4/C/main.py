79bebe3998988413f08a752222d55961c0f0f8b9
Niklas Jönsson <niklasjn@live.se>
2017-12-09 15:20:51 +0100

    lil update

--- Assignments/Assignment4/C/main.py
+++ Assignments/Assignment4/C/main.py
@@ -9,49 +9,11 @@ def shared_passphrase(g_xy):
 def smp_format(b, g, p):
     return format(pow(g, b, p), 'x')
 
+def encrypt(msg, secret):
+    return int(msg, 16) ^ int(secret, 16)
 
-def smp(soc, g1, p, y):
-    g1_a2 = int(soc.recv(4096).decode('utf8').strip(), 16)
-    print("received g1_a2")
-
-    b2 = random.randint(40000000, 50000000)
-    soc.send(smp_format(b2, g1, p).encode('utf8'))
-    print('sent g1_b2: ', soc.recv(4096).decode('utf8').strip())
-    g2 = pow(g1_a2, b2, p)
-
-    g1_a3 = int(soc.recv(4096).decode('utf8').strip(), 16)
-    print("received g1_a3")
-
-    b3 = random.randint(40000000, 50000000)
-    soc.send(smp_format(b3, g1, p).encode('utf8'))
-    print('sent g1_b3: ', soc.recv(4096).decode('utf8').strip())
-
-    g3 = pow(g1_a3, b3, p)
-
-    Pa = int(soc.recv(4096).decode('utf8').strip(), 16)
-    print("received Pa")
-
-    b = random.randint(40000000, 50000000)
-    y = int(y, 16) #ytterst oklart
-
-    Pb, Qb = pow(g3, b, p), pow(g1, b, p) * pow(g2, y, p)
-    soc.send(format(Pb, 'x').encode('utf8'))
-    print('sent Pb: ', soc.recv(4096).decode('utf8').strip())
-
-    Qa = int(soc.recv(4096).decode('utf8').strip(), 16)
-    print("received Qa")
-
-    soc.send(format(Qb, 'x').encode('utf8'))
-    print('sent Qb: ', soc.recv(4096).decode('utf8').strip())
-
-    QaQb_inv_a3 = int(soc.recv(4096).decode('utf8').strip(), 16)
-    print("received QaQb_inv_a3")
-
-    QaQb_inv_b3 = pow(Qa*modinv(Qb, p), b3, p)
-    soc.send(format(QaQb_inv_b3, 'x').encode('utf8'))
-    print('sent QaQb_inv_b3: ', soc.recv(4096).decode('utf8').strip())
-    print('Authentication: ', soc.recv(4096).decode('utf8').strip())
-
+def decrypt(msg, secret):
+    return int(msg, 16) ^ int(secret, 16)
 
 def dh_keyexchange(soc, p, g):
     ##########################
@@ -61,7 +23,6 @@ def dh_keyexchange(soc, p, g):
     ## receive g**x1
     # receive the hex-string, decode, and remove trailing '\n'
     g_x1 = int(soc.recv(4096).decode('utf8').strip(), 16)
-    print("received g_x1")
     # generate g**x2, x2 shall be a random number
     x2 = random.randint(4000000000000, 5000000000000)
     # calculate g**x2 mod p
@@ -70,9 +31,8 @@ def dh_keyexchange(soc, p, g):
     # convert to hex-string
     g_x2_str = format(g_x2, 'x')
     # send it
-    soc.send(g_x2_str.encode('utf8'))
-    # read the ack/nak. This should yield a nak due to x2 being 0
-    print ('sent g_x2:', soc.recv(4096).decode('utf8').strip())
+    send(soc, g_x2, "g_x2")
+    print("*** key exchange complete ***")
     return pow(g_x1, x2, p)
 
 
@@ -90,6 +50,52 @@ def modinv(a, m):
     else:
         return x % m
 
+
+def send(soc, nbr, text):
+    soc.send(format(nbr, 'x').encode('utf8'))
+    print('sent ' + text + ': ', soc.recv(4096).decode('utf8').strip())
+
+def receive(soc):
+    return soc.recv(4096).decode('utf8').strip()
+
+def receive_nbr(soc):
+    return int(receive(soc), 16)
+
+def smp_1(soc, g1, p, y):
+    y = int(y, 16)
+    b2 = random.randint(4000000000000, 5000000000000)
+    b3 = random.randint(4000000000000, 5000000000000)
+    r = random.randint(4000000000000, 5000000000000)
+
+    g1_a2 = receive_nbr(soc)
+    g1_b2 = pow(g1, b2, p)
+    send(soc, g1_b2, "g1_b2")
+
+    g2 = pow(g1_a2, b2, p)
+
+
+    g1_a3 = receive_nbr(soc)
+    g1_b3 = pow(g1, b3, p)
+    send(soc, g1_b3, "g1_b3")
+
+    g3 = pow(g1_a3, b3, p)
+    Pa = receive_nbr(soc)
+    Pb = pow(g3, r, p)
+    Qb = pow(g1, r, p) * pow(g2, y, p)
+
+    send(soc, Pb, "Pb")
+    Qa = receive_nbr(soc)
+
+    send(soc, Qb, "Qb")
+    QaQb_inv_a3 = receive_nbr(soc)
+    QaQb_inv_b3 = pow(Qa * modinv(Qb, p), b3, p)
+    send(soc, QaQb_inv_b3, "QaQb_inv_b3")
+
+
+
+    print("*** auth = {} ***".format(receive(soc)))
+
+    #Send Rb
 def main():
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     soc.connect(("eitn41.eit.lth.se", 1337))
@@ -98,9 +104,11 @@ def main():
         p = int(content_file.read().replace('\n', '').replace(' ', ''), 16)
     g = g1 = 2 # g = g1 = 2
 
-    secret = shared_passphrase(dh_keyexchange(soc, p, g))
+    g_x1x2 = dh_keyexchange(soc, p, g)
+    secret = shared_passphrase(g_x1x2)
 
-    smp(soc, g1, p, secret)
+    #smp(soc, g1, p, secret)
+    smp_1(soc, g1, p, secret)
 
 if __name__ == "__main__":
     main()
